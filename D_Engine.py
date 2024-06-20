003eae357f45f17e3b7b76ad99d73a243753ea3c
Dheer20 <133756135+Dheer20@users.noreply.github.com>
2024-06-20 20:51:16 +0530

    Geometry functions created & Code Organized
    added Matrix , Vector & Triangle methods for ease of code writing and readability

--- D_Engine.py
+++ D_Engine.py
@@ -1,6 +1,7 @@
-import pygame 
+import pygame  # type: ignore
 from sys import exit
 import math as m
+from Geometry import *
 
 #Declaring Constants 
 SCREEN_COLOR = "#171717"
@@ -8,93 +9,19 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 SCREEN_SIZE = (SCREEN_WIDTH,SCREEN_HEIGHT)
 LINE_COLOR = "#A9DDD6"
-LINE_THICKNESS = 3
-FACE_COLOR = "#DDDDDD"
-A = SCREEN_WIDTH/SCREEN_HEIGHT
-FOV = 90.0 # in Degrees
-Zn = 0.1
-Zf = 1000.0
-P = 1/m.tan(m.radians(FOV)/2)
-T = Zf/(Zf-Zn)
-
-
-# Defining Functions and Objects
-class Vector():
-    def __init__(self,x,y,z) -> None:
-        self.x = float(x) 
-        self.y = float(y) 
-        self.z = float(z)
-
-class Triangle():
-    def __init__(self, v0, v1, v2) -> None:
-        self.v0 = Vector(*v0)
-        self.v1 = Vector(*v1)
-        self.v2 = Vector(*v2)
-        self.color = (12,103,310)
-        self.order = 1
-    
-    def GetColor(self,intensity) -> None:
-        color_value = int(intensity * 255)
-        self.color = (color_value, color_value, color_value)
-
-class Mesh_3D():
-    def __init__(self,triangles) -> None:
-        self.triangles = triangles
-
-    def LoadObjFile(filename):
-        vertices = []
-        triangles = []
-
-        with open(filename, 'r') as file:
-            for line in file:
-                # Ignore comments and empty lines
-                if line.startswith('#') or line.strip() == '':
-                    continue
-
-                # Parse vertex lines
-                if line.startswith('v '):
-                    parts = line.split()
-                    x, y, z = float(parts[1]), float(parts[2]), float(parts[3])
-                    vertex = (x, y, z)  # Store vertices as iterable objects
-                    vertices.append(vertex)
-
-                # Parse face lines
-                elif line.startswith('f '):
-                    parts = line.split()
-                    vertex_indices = [int(part.split('/')[0]) - 1 for part in parts[1:4]]  # -1 to convert to zero-based index
-                    v0, v1, v2 = vertices[vertex_indices[0]], vertices[vertex_indices[1]], vertices[vertex_indices[2]]
-                    triangle = Triangle(v0, v1, v2)
-                    triangles.append(triangle)
-
-        return Mesh_3D(triangles)
+LINE_THICKNESS = 1
 
 #Declaring Variables
 theta1,theta2 = 0,0
 
-# Defining Matrices
-Mat_Proj = [P/A,0,0,0
-            ,0,P,0,0
-            ,0,0,T,-Zf*T
-            ,0,0,1,0 ]
+# Creating Matrices
+Mat_Proj=Matrix_3D
+Mat_Proj=Matrix_3D.Projection(A=SCREEN_WIDTH/SCREEN_HEIGHT,FOV=120.0,Zn=0.01,Zf=1000.0)
 
 # Defining Characteristic Vectors
 Camera_3D = Vector(0,0,0)
 Light_dir = Vector(0,0,-1)
-len = (Light_dir.x**2 + Light_dir.y**2 + Light_dir.z**2) ** 0.5
-if len != 0:
-    Light_dir.x /= len
-    Light_dir.y /= len
-    Light_dir.z /= len
-
-def MatVectorMul(i,m,o) -> None:# i is input vector , m is 4x4 matrix to be multiplied , o is the output vector
-    
-    o.x = i.x * m[0] + i.y * m[1] + i.z * m[2] + m[3]
-    o.y = i.x * m[4] + i.y * m[5] + i.z * m[6] + m[7]
-    o.z = i.x * m[8] + i.y * m[9] + i.z * m[10] + m[11]
-    w = float(i.x * m[12] + i.y * m[13] + i.z * m[14] + m[15])
-    
-    if w != 0:
-        o.x /= w ; o.y /= w ; o.z /= w
+Light_dir.Normalize()
 
 # Setting up the Pygame Window/Screen
 pygame.init()
@@ -103,22 +30,8 @@ pygame.display.set_caption("3D Engine")
 clock=pygame.time.Clock()
 
 # Loading Object Data
-tri1 = Triangle((0,0,0),(0,1,0),(1,1,0))
-tri2 = Triangle((0,0,0),(1,1,0),(1,0,0))
-tri3 = Triangle((1,0,0),(1,1,0),(1,1,1))
-tri4 = Triangle((1,0,0),(1,1,1),(1,0,1))
-tri5 = Triangle((1,0,1),(1,1,1),(0,1,1))
-tri6 = Triangle((1,0,1),(0,1,1),(0,0,1))
-tri7 = Triangle((0,0,1),(0,1,1),(0,1,0)) 
-tri8 = Triangle((0,0,1),(0,1,0),(0,0,0))
-tri9 = Triangle((0,1,0),(0,1,1),(1,1,1))
-tri10 = Triangle((0,1,0),(1,1,1),(1,1,0))
-tri11 = Triangle((1,0,1),(0,0,1),(0,0,0))
-tri12 = Triangle((1,0,1),(0,0,0),(1,0,0))
 
-Cube_3D = Mesh_3D((tri1,tri2,tri3,tri4,tri5,tri6,tri7,tri8,tri9,tri10,tri11,tri12))
 Monkey = Mesh_3D.LoadObjFile("monkey.obj")
-
 
 # Engine Update Loop
 while True:
@@ -142,23 +55,11 @@ while True:
 
     theta1 += 0.03  
     theta2 += 0.01
+    
+    Mat_X_Rot = Matrix_3D.X_Rotation(theta1)
+    Mat_Y_Rot = Matrix_3D.Y_Rotation(theta2)
+    Mat_Z_Rot = Matrix_3D.Z_Rotation(theta1)
 
-    Mat_X_Rot = [1,0,0,0
-            ,0,m.cos(theta1),-m.sin(theta1),0
-            ,0,m.sin(theta1),m.cos(theta1),0
-            ,0,0,0,1 ]
-
-    Mat_Y_Rot = [
-                m.cos(theta2),0,m.sin(theta2),0
-                ,0,1,0,0
-                ,-m.sin(theta2),0,m.cos(theta2),0
-                ,0,0,0,1 ]
-        
-    Mat_Z_Rot = [m.cos(theta2),-m.sin(theta2),0,0
-                ,m.sin(theta2),m.cos(theta2),0,0
-                ,0,0,1,0
-                ,0,0,0,1 ]
-        
     # Applying Transformations
     TriangleToRasterList=[]
 
@@ -167,32 +68,26 @@ while True:
     for tri in Monkey.triangles:
         
         # Creating Triangle objects
-        Projected_tri = Triangle((1,0,0),(0,1,0),(0,0,1))
-        X_Rotated_tri = Triangle((1,0,0),(0,1,0),(0,0,1))
-        ZX_Rotated_tri = Triangle((1,0,0),(0,1,0),(0,0,1))
-        YZX_Rotated_tri = Triangle((1,0,0),(0,1,0),(0,0,1))
+        # Projected_tri = Triangle()
+        # X_Rotated_tri = Triangle()
+        # ZX_Rotated_tri = Triangle()
+        # YZX_Rotated_tri = Triangle()
         
         # Appling multiplications for rotation
         # Rotate in X-axis
-        MatVectorMul(tri.v0,Mat_X_Rot,X_Rotated_tri.v0)
-        MatVectorMul(tri.v1,Mat_X_Rot,X_Rotated_tri.v1)
-        MatVectorMul(tri.v2,Mat_X_Rot,X_Rotated_tri.v2)
+        X_Rotated_tri=Matrix_3D.MatTriMul(tri,Mat_X_Rot)
 
         # Rotate in Z-axis
-        MatVectorMul(X_Rotated_tri.v0,Mat_Z_Rot,ZX_Rotated_tri.v0)
-        MatVectorMul(X_Rotated_tri.v1,Mat_Z_Rot,ZX_Rotated_tri.v1)
-        MatVectorMul(X_Rotated_tri.v2,Mat_Z_Rot,ZX_Rotated_tri.v2)
+        ZX_Rotated_tri=Matrix_3D.MatTriMul(X_Rotated_tri,Mat_Z_Rot)
 
         # Rotate in Y-axis
-        MatVectorMul(ZX_Rotated_tri.v0,Mat_Y_Rot,YZX_Rotated_tri.v0)
-        MatVectorMul(ZX_Rotated_tri.v1,Mat_Y_Rot,YZX_Rotated_tri.v1)
-        MatVectorMul(ZX_Rotated_tri.v2,Mat_Y_Rot,YZX_Rotated_tri.v2)
+        YZX_Rotated_tri=Matrix_3D.MatTriMul(ZX_Rotated_tri,Mat_Y_Rot)
 
         #Translating Triangles along the Z-axis
         Translated_tri = YZX_Rotated_tri
-        Translated_tri.v0.z += 3.0
-        Translated_tri.v1.z += 3.0
-        Translated_tri.v2.z += 3.0
+        Translated_tri.v0.z += 2.0
+        Translated_tri.v1.z += 2.0
+        Translated_tri.v2.z += 2.0
 
         # Calculating Normals
         line1 = Vector(Translated_tri.v1.x - Translated_tri.v0.x, 
@@ -203,22 +98,16 @@ while True:
                        Translated_tri.v2.y - Translated_tri.v0.y, 
                        Translated_tri.v2.z - Translated_tri.v0.z)
 
-        normal = Vector(line1.y * line2.z - line1.z * line2.y,
-                        line1.z * line2.x - line1.x * line2.z,
-                        line1.x * line2.y - line1.y * line2.x)
+        normal = line1 @ line2
 
-        len = (normal.x**2 + normal.y**2 + normal.z**2) ** 0.5
-        if len != 0:
-            normal.x /= len
-            normal.y /= len
-            normal.z /= len
+        normal.Normalize()
         
         #Calculating Culling conditon
         view_direction = Vector(Translated_tri.v0.x - Camera_3D.x,
                         Translated_tri.v0.y - Camera_3D.y,
                         Translated_tri.v0.z - Camera_3D.z)
 
-        dot_product = normal.x * view_direction.x + normal.y * view_direction.y + normal.z * view_direction.z
+        dot_product = normal ** view_direction
 
         if dot_product < 0.0:  #backface culling condition
 
@@ -228,9 +117,7 @@ while True:
             Translated_tri.GetColor(Illumination_dp)
 
             #Projecting the Triangles
-            MatVectorMul(Translated_tri.v0,Mat_Proj,Projected_tri.v0)
-            MatVectorMul(Translated_tri.v1,Mat_Proj,Projected_tri.v1)
-            MatVectorMul(Translated_tri.v2,Mat_Proj,Projected_tri.v2)
+            Projected_tri = Matrix_3D.MatTriMul(Translated_tri,Mat_Proj)
             Projected_tri.color = Translated_tri.color
 
             #Scaling the triangles
---
+++ Geometry.py
@@ -0,0 +1,197 @@
+import math as m
+
+# Defining Functions and Objects
+class Vector():
+    def __init__(self,x=0,y=0,z=0,w=1) -> None:
+        self.x = float(x)
+        self.y = float(y) 
+        self.z = float(z)
+        self.w = float(w)
+
+    def Normalize(self):
+        len = (self.x**2 + self.y**2 + self.z**2) ** 0.5
+        if len != 0:
+            self.x /= len
+            self.y /= len
+            self.z /= len
+    
+    def Make3D(self):
+        if self.w != 0:    
+            self.x /= self.w
+            self.y /= self.w
+            self.z /= self.w
+            self.w /= self.w
+        else: print('W is 0')
+
+    def __matmul__(self,other:'Vector')-> 'Vector': #CROSS PRODUCT
+        return Vector(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x)
+    
+    def __mul__(self,other: float)-> 'Vector':
+        return Vector(self.x * other ,self.y * other ,self.z * other)
+
+    def __pow__(self,other:'Vector')-> float: #DOT PRODUCT
+        return self.x*other.x+self.y*other.y+self.z*other.z
+    
+    def __str__(self):
+        self.Make3D
+        return f"({self.x},{self.y},{self.z})"
+
+    def __repr__(self):
+        return f"Vector(x={self.x}, y={self.y}, z={self.z}, w={self.w})"
+ 
+class Triangle():
+    def __init__(self, v0=(1,1,1), v1=(2,2,2), v2=(3,3,3) ,color=None ,order=None)  -> None:
+        self.v0 = Vector(*v0)
+        self.v1 = Vector(*v1)
+        self.v2 = Vector(*v2)
+        self.color = color
+        self.order = order
+    
+    def GetColor(self,intensity) -> None:
+        color_value = int(intensity * 255)
+        self.color = (color_value,color_value ,color_value)
+
+class Matrix_3D():
+    def __init__(self,matrix=[[0,0,0,0] for _ in range(4)]) -> None:
+        self.matrix = matrix
+
+    def __add__(self,other: 'Matrix_3D')-> 'Matrix_3D':
+        result = Matrix_3D()
+        for i in range(4):
+            for j in range(4):
+                result.matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
+        return result
+
+    __radd__ = __add__
+    
+    def __sub__(self,other :'Matrix_3D')-> 'Matrix_3D':
+        result = Matrix_3D()
+        for i in range(4):
+            for j in range(4):
+                result.matrix[i][j] = self.matrix[i][j] - other.matrix[i][j]
+        return result
+    
+    __rsub__ = __sub__
+    
+    def __mul__(self,other: float)-> 'Matrix_3D':
+        result = Matrix_3D()
+        for i in range(4):
+            for j in range(4):
+                result.matrix[i][j] = self.matrix[i][j] * other
+        return result
+
+    def __matmul__(self,other:'Matrix_3D')-> 'Matrix_3D':
+        result = Matrix_3D()
+        for i in range(4):
+            for j in range(4):
+                temp=0
+                for k in range(4):
+                    temp += self.matrix[i][k] * other.matrix[k][j]
+                result.matrix[i][j] = temp
+        return result
+        
+    def __str__(self):
+        matrix_str = ""
+        for row in self.matrix:
+            matrix_str += ' '.join(map(str, row)) + '\n'
+        return matrix_str
+    
+    @classmethod
+    def Identity(self):
+        return self([[1,0,0,0],
+                    [0,1,0,0],
+                    [0,0,1,0],
+                    [0,0,0,1]])
+
+    @classmethod
+    def Projection(self,A,FOV,Zn,Zf)->list:
+        T = Zf/(Zf-Zn)
+        P = 1/m.tan(m.radians(FOV)/2)
+        return self ([[P/A,0,0,0]
+                    ,[0,P,0,0]
+                    ,[0,0,T,-Zf*T]
+                    ,[0,0,1,0]])
+
+    
+    @classmethod
+    def X_Rotation(self,Angle):
+        return self([[1,0,0,0]
+                    ,[0,m.cos(Angle),-m.sin(Angle),0]
+                    ,[0,m.sin(Angle),m.cos(Angle),0]
+                    ,[0,0,0,1]])
+
+        
+    @classmethod
+    def Y_Rotation(self,Angle):
+        return self([[m.cos(Angle),0,m.sin(Angle),0]
+                    ,[0,1,0,0]
+                    ,[-m.sin(Angle),0,m.cos(Angle),0]
+                    ,[0,0,0,1]])
+
+
+    @classmethod
+    def Z_Rotation(self,Angle):
+        return self([[m.cos(Angle),-m.sin(Angle),0,0]
+                    ,[m.sin(Angle),m.cos(Angle),0,0]
+                    ,[0,0,1,0]
+                    ,[0,0,0,1]])
+    
+    @staticmethod
+    def MatVectorMul(i,m) -> Vector:# i is input vector , m is 4x4 matrix to be multiplied
+        m = m.matrix
+        result = Vector()
+        result.x = i.x * m[0][0] + i.y * m[0][1] + i.z * m[0][2] + m[0][3]
+        result.y = i.x * m[1][0] + i.y * m[1][1] + i.z * m[1][2] + m[1][3]
+        result.z = i.x * m[2][0] + i.y * m[2][1] + i.z * m[2][2] + m[2][3]
+        result.w = float(i.x * m[3][0] + i.y * m[3][1] + i.z * m[3][2] + m[3][3])
+        Vector.Make3D(result)
+        return result
+    
+    @staticmethod
+    def MatTriMul(t,m)-> Triangle:
+        result = Triangle()
+        result.v0 = Matrix_3D.MatVectorMul(t.v0,m)
+        result.v1 = Matrix_3D.MatVectorMul(t.v1,m)
+        result.v2 = Matrix_3D.MatVectorMul(t.v2,m)
+        return result
+        
+
+class Mesh_3D():
+    def __init__(self,triangles) -> None:
+        self.triangles = triangles
+
+    def LoadObjFile(filename):
+        vertices = []
+        triangles = []
+
+        with open(filename, 'r') as file:
+            for line in file:
+                # Ignore comments and empty lines
+                if line.startswith('#') or line.strip() == '':
+                    continue
+
+                # Parse vertex lines
+                if line.startswith('v '):
+                    parts = line.split()
+                    x, y, z = float(parts[1]), float(parts[2]), float(parts[3])
+                    vertex = (x, y, z)  # Store vertices as iterable objects
+                    vertices.append(vertex)
+
+                # Parse face lines
+                elif line.startswith('f '):
+                    parts = line.split()
+                    vertex_indices = [int(part.split('/')[0]) - 1 for part in parts[1:4]]  # -1 to convert to zero-based index
+                    v0, v1, v2 = vertices[vertex_indices[0]], vertices[vertex_indices[1]], vertices[vertex_indices[2]]
+                    triangle = Triangle(v0, v1, v2)
+                    triangles.append(triangle)
+
+        return Mesh_3D(triangles)
+    
+if __name__ == '__main__':
+    A = Vector(2,1,1)
+    B= Vector(1,1,1)
+    C = A * 5
+    print(C)
